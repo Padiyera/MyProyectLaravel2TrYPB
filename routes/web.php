65522bb7130667bc1ec51ff7eeea55864207f3d5
Padiyera <ypadbor947@g.educaand.es>
2025-03-01 18:24:22 +0100

    mejora enlace

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect()->route('dashboard');
+})->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
